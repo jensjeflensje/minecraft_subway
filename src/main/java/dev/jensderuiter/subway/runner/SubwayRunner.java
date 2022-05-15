ef6e46d04aec0108da7b86ac3ed240822a975adb
Jens <jensgamesnl@gmail.com>
2022-05-15 12:16:13 +0200

    Make tram turn smoother

--- src/main/java/dev/jensderuiter/subway/runner/SubwayRunner.java
+++ src/main/java/dev/jensderuiter/subway/runner/SubwayRunner.java
@@ -2,6 +2,7 @@ package dev.jensderuiter.subway.runner;
 
 import dev.jensderuiter.subway.track.PassingPoint;
 import dev.jensderuiter.subway.type.Subway;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Sound;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -11,15 +12,21 @@ public class SubwayRunner extends BukkitRunnable {
 
     private Subway subway;
     private PassingPoint lastPoint;
+    private Vector lastVector;
+    private int countSameVector = 0;
     private int index = 1;
     private int STOPTICKS = 100;
+    private float TURNSPEED = 3.5f;
     private int stopTicks = 100;
     private boolean stopped = false;
     private float soundTotalTicks;
     private float soundTicks = -1;
+    private float aimedYaw = 0;
+    private float currentYaw = -420;
 
     public SubwayRunner(Subway subway, Location location) {
         this.subway = subway;
+        this.TURNSPEED = this.TURNSPEED + (this.subway.getSpeed() * 7);
         this.lastPoint = new PassingPoint(false, location);
     }
 
@@ -63,8 +70,30 @@ public class SubwayRunner extends BukkitRunnable {
 
         if (!this.stopped) {
             Vector vector = nextPoint.location.toVector().subtract(lastPoint.location.toVector());
+
+            this.aimedYaw = (float) (Math.atan2(vector.getZ(), vector.getX()) * 180 / Math.PI) - 90;
+            if (this.currentYaw == -420) this.currentYaw = aimedYaw;
+            if (this.currentYaw + TURNSPEED < aimedYaw) {
+                if (this.currentYaw < -180) this.currentYaw = 90;
+                this.currentYaw += TURNSPEED;
+            } else if (this.currentYaw - TURNSPEED > aimedYaw) {
+                this.currentYaw -= TURNSPEED;
+            }
+
+            // make the subway turn straight on straight movement
+            if (this.lastVector != null && this.lastVector.normalize().equals(vector.normalize())) {
+                this.countSameVector++;
+                if (this.countSameVector >= 20) {
+                    this.currentYaw = aimedYaw;
+                }
+            } else {
+                this.countSameVector = 0;
+            }
+
+            this.lastPoint.location.setYaw(this.currentYaw);
+
             this.lastPoint.location = lastPoint.location.add(vector.normalize().multiply(subway.getSpeed()));
-            this.lastPoint.location.setYaw((float) (Math.atan2(vector.getZ(), vector.getX()) * 180 / Math.PI) - 90);
+            this.lastVector = vector;
 
         }
         this.subway.move(this.lastPoint.location);
@@ -75,6 +104,6 @@ public class SubwayRunner extends BukkitRunnable {
     }
 
     private void playStopSound() {
-        this.lastPoint.location.getWorld().playSound(this.lastPoint.location, Sound.BLOCK_ANVIL_PLACE, 0.6f, 0);
+        this.lastPoint.location.getWorld().playSound(this.lastPoint.location, Sound.BLOCK_ANVIL_PLACE, 0.4f, 0);
     }
 }
